3e7e4da723505d071c949c36fd4b3c722e939424
kaha <kokhanenko.s@gmail.com>
2016-11-17 18:31:08 +0200

    implement jersey

---
+++ src/main/java/ua/pp/kaha/Service.java
@@ -0,0 +1,21 @@
+package ua.pp.kaha;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by skokhanenko on 17.11.2016.
+ */
+public class Service {
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+}
