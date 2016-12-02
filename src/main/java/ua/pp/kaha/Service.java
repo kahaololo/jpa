5b76115722b1e49b392810397a910d6ffea58d49
kaha <kokhanenko.s@gmail.com>
2016-12-02 18:22:43 +0200

    implement table

--- src/main/java/ua/pp/kaha/Service.java
+++ src/main/java/ua/pp/kaha/Service.java
@@ -3,15 +3,16 @@ package ua.pp.kaha;
 import ua.pp.kaha.model.Measurement;
 import ua.pp.kaha.model.User;
 
-import javax.persistence.*;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by skokhanenko on 17.11.2016.
@@ -20,9 +21,9 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Service {
 
     @GET
-    @Path("/{user}/measurements")
+    @Path("/measurements")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getMsg(@PathParam("user") String userName) throws Exception {
+    public Response getMsg(@QueryParam("user") String userName) throws Exception {
         List<Measurement> measurements = new ArrayList<Measurement>();
         EntityManager em = EMFListener.createEntityManager();
 
@@ -42,32 +43,64 @@ public class Service {
         return Response.status(200).entity(measurements).build();
     }
 
-    @GET
-    @Path("/put/measurement")
+//    @GET
+//    @Path("/put/measurement")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response putMeasurement() {
+//        Map<String, String> rs = new HashMap<String, String>();
+//
+//        EntityManager em = EMFListener.createEntityManager();
+//
+//        User user = em.find(User.class, 1);
+//        Query query = em.createQuery("SELECT max(m.measurementId.date) FROM Measurement m WHERE m.measurementId.userId = :userId");
+//        query.setParameter("userId", user.getId());
+//        Date maxDate = (Date) query.getSingleResult();
+//        Date nextDate = new Date(maxDate.getTime() + (1000 * 60 * 60 * 24));
+//        int weight = ThreadLocalRandom.current().nextInt(80, 100);
+//        int waist = ThreadLocalRandom.current().nextInt(80, 90);
+//        user.addMeasurment(nextDate, weight, waist);
+//
+//        EntityTransaction tx = em.getTransaction();
+//        tx.begin();
+//        em.merge(user);
+//        tx.commit();
+//
+//        em.close();
+//
+//        rs.put("result", "200");
+//
+//        return Response.status(200).entity(rs).build();
+//    }
+
+    @POST
+    @Path("/measurements")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response putMeasurement() {
+    public void addMeasurements(
+            @FormParam("user") String user,
+            @FormParam("measurement") Object measurement
+    ) {
+
+
         Map<String, String> rs = new HashMap<String, String>();
 
-        EntityManager em = EMFListener.createEntityManager();
+//        EntityManager em = EMFListener.createEntityManager();
+//
+//        User user = em.find(User.class, 1);
+//        Query query = em.createQuery("SELECT max(m.measurementId.date) FROM Measurement m WHERE m.measurementId.userId = :userId");
+//        query.setParameter("userId", user.getId());
+//        Date maxDate = (Date) query.getSingleResult();
+//        Date nextDate = new Date(maxDate.getTime() + (1000 * 60 * 60 * 24));
+//        int weight = ThreadLocalRandom.current().nextInt(80, 100);
+//        int waist = ThreadLocalRandom.current().nextInt(80, 90);
+//        user.addMeasurment(nextDate, weight, waist);
+//
+//        EntityTransaction tx = em.getTransaction();
+//        tx.begin();
+//        em.merge(user);
+//        tx.commit();
+//
+//        em.close();
 
-        User user = em.find(User.class, 1);
-        Query query = em.createQuery("SELECT max(m.measurementId.date) FROM Measurement m WHERE m.measurementId.userId = :userId");
-        query.setParameter("userId", user.getId());
-        Date maxDate = (Date) query.getSingleResult();
-        Date nextDate = new Date(maxDate.getTime() + (1000 * 60 * 60 * 24));
-        int weight = ThreadLocalRandom.current().nextInt(80, 100);
-        int waist = ThreadLocalRandom.current().nextInt(80, 90);
-        user.addMeasurment(nextDate, weight, waist);
-
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-        em.merge(user);
-        tx.commit();
-
-        em.close();
-
-        rs.put("result", "200");
-
-        return Response.status(200).entity(rs).build();
     }
 }
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,52 +1,102 @@
+
 $(function() {
 
-    //var login = getCookie("login");
+    var login = "Kaha";
+    loadUserChart(login);
 
-    showUserChart("Kaha");
+    $("#table").on('click', '.remove', removeMeasurement);
+    $("#table").on('click', '.edit', editMeasurement);
+    $("#table").on('click', '.edit', addMeasurement);
+
+
 });
 
+function addMeasurement() {
+    var tr = $(this).parent().parent();
 
-function showUserChart (login) {
-    $.getJSON(
-        "/service/"+login+"/measurements",
-        function( data ) {
-            console.log(data);
-            var chartDataWeight = [];
-            var chartDataWaist = [];
-            //
-            for (var i in data) {
-                var routine = data[i];
-                chartDataWeight.push( [new Date(routine.measurementId.date).getTime(), routine.weight ] );
-                chartDataWaist.push( [new Date(routine.measurementId.date).getTime(), routine.waist ] );
-            }
-            //
-            $('#chart').highcharts({
-                chart: {
-                    type: 'spline',
-                },
-                title: {
-                    text: '%UserName% chart',
-                },
-                xAxis: {
-                    type: 'datetime',
-                    dateTimeLabelFormats: {
-                        day: '%b %e'
-                    },
-                },
-                yAxis: {
-                    title: {
-                        text: 'Kg/Cm'
-                    },
-                },
-                series: [{
-                    name: 'Weight',
-                    data: chartDataWeight
-                }, {
-                    name: 'Waist',
-                    data: chartDataWaist
-                }]
-            });
+    var measurement = {
+        date: $(tr).children().eq(0).children("input").val(),
+        weight: $(tr).children().eq(1).children("input").val(),
+        waist: $(tr).children().eq(2).children("input").val()
+    };
 
+    var request = $.ajax({
+        method: "POST",
+        url: "/service/measurements/",
+        data: {
+            user: login,
+            measurement: measurement
         }
-    );
+    });
+
+    request.done(function (message) {
+        console.log(data);
+        console.log(message);
+    });
+
+}
+
+function removeMeasurement() {
+    $(this).parent().parent().fadeOut(350, function(){
+        $(this).remove();
+    });
+}
+
+function editMeasurement() {
+    var tr = $(this).parent().parent();
+}
+
+function loadUserChart (login) {
+
+    var request = $.ajax({
+        method: "GET",
+        url: "/service/measurements/",
+        data: {
+            user: login
+        }
+    });
+
+    request.done(function (data) {
+        //render table template
+        var source   = $("#table-template").html();
+        var template = Handlebars.compile(source);
+        $("#table").html(template(data));
+
+        var chartDataWeight = [];
+        var chartDataWaist = [];
+        //
+        for (var i in data) {
+            var routine = data[i];
+            chartDataWeight.push( [new Date(routine.measurementId.date).getTime(), routine.weight ] );
+            chartDataWaist.push( [new Date(routine.measurementId.date).getTime(), routine.waist ] );
+        }
+        //
+        $('#chart').highcharts({
+            chart: {
+                type: 'spline'
+            },
+            title: {
+                text: '%UserName% chart'
+            },
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: {
+                    day: '%b %e'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Kg/Cm'
+                }
+            },
+            series: [{
+                name: 'Weight',
+                data: chartDataWeight
+            }, {
+                name: 'Waist',
+                data: chartDataWaist
+            }]
+        });
+
+    });
 }
